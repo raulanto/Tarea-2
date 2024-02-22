f8bd1db9aa0d6e83f325483082cd7abffcf957d7
raul <74162376+raulanto@users.noreply.github.com>
2024-02-21 22:16:02 -0600

    cambios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "index.html",
-    "funciones.js",
-    "main.js",
     "./node_modules/flowbite/**/*.js",
     "./src/**/*.{html,js}",
   ],
